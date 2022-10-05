fc4a4e08f05970f55a4cc0353c09a61019bf84ca
Sasha <nedakerrr@gmail.com>
2022-10-05 16:16:24 +0300

    риятный код 2

--- blue_id.py
+++ blue_id.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
-
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 while (1):
     ret, frame = cap.read()
@@ -21,6 +24,9 @@ while (1):
         nzCount = cv2.countNonZero(mask);
         break
 print(nzCount)
+print(width, height)
+if nzCount > width * height / 3:
+    print("WELL DONE")
 
 cv2.destroyAllWindows()
 
