d979c02a8a864b0891c24217a5e3e028ca1f90fc
m3b3s <buzenkovms@gamil.com>
2022-10-19 15:17:12 +0300

    pizza

--- aruco.py
+++ aruco.py
@@ -1,22 +1,43 @@
+import time
 import cv2
+import numpy as np
+from pioneer_sdk import Camera, Pioneer
 
-# Словарь aruco-маркеров
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-# Параметры детектирования (в данном случае параметры по умолчанию)
 arucoParams = cv2.aruco.DetectorParameters_create()
 
-cap = cv2.VideoCapture(0)
+camera = Camera()
+pioneer = Pioneer()
+curr_time = time.time()
+delta_time=0.05
 
 while True:
-    ret, frame = cap.read()  # Получаем изображение
-    # Детектируем маркеры
-    corners, ids, rejected = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
-    # Обводим распознанные маркеры на изображении
-    cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-    cv2.imshow('video', frame)  # Выводим изображение на экран
+    raw_frame = camera.get_frame()  # Получаем сырые данные
+    if raw_frame is not None:
+        # Декодируем полученные данные, чтобы получить изображение
+        frame = camera_frame = cv2.imdecode(np.frombuffer(raw_frame, dtype=np.uint8), cv2.IMREAD_COLOR)
+        corners, ids, rejected = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+
+        if time.time() - curr_time > delta_time:
+            for i in range(4):
+                if ids and 4 in ids:
+                    pioneer.led_control(i, 255, 0, 0)
+                    pioneer.arm()
+                    pioneer.takeoff()
+                else:
+                    pioneer.led_control(i, 0, 0, 255)
+                if ids and 5 in ids:
+                    pioneer.land()
+                    pioneer.disarm()
+            curr_time = time.time()
+
+        # Обводим распознанные маркеры на изображении
+        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+        cv2.imshow('video', frame)  # Выводим изображение на экран
 
     if cv2.waitKey(1) & 0xFF == 27:  # Выход из программы, если нажали ESC
+        for i in range(4):
+            pioneer.led_control(i, 0, 0, 0)
         break
 
-cap.release()  # Отпускаем захват камеры
 cv2.destroyAllWindows()  # Закрываем все открытые окна
