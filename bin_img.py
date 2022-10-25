11a81e5dfd12fabb04a6f81b61c2eab060f4e06a
kefur <3939723@gmail.com>
2022-10-26 00:46:22 +0300

    создание и обрабока бин изображения

---
+++ bin_img.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+# cap.set(3, 800)
+# cap.set(4, 600)
+
+while True:
+    success, img = cap.read()
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # перевод ч/б картинку
+
+    img = cv2.Canny(img, 50, 50) # создание бинарного изображения
+
+    kernel = np.ones((5,5), np.uint8) # создание матрицы состоящей из единиц
+    img = cv2.dilate(img, kernel, iterations=1) # расширяем точки
+    img = cv2.erode(img, kernel, iterations=1) # эрозия
+    cv2.imshow('test', img)
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == 27:
+        break
