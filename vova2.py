b86b3b7781032f05ca153bed7f00ff78456af41b
vov4k <golovach.volodia@yandex.ru>
2022-11-23 15:55:53 +0300

    для Саша)

--- vova2.py
+++ vova2.py
@@ -4,47 +4,56 @@ import cv2
 import numpy as np
 from pioneer_sdk import Camera, Pioneer
 
-drone_ips = ["192.168.137.109"]
-cameras = [Camera(ip, ip=ip) for ip in drone_ips]
-pioneers = [Pioneer(ip, ip=ip) for ip in drone_ips]
+drone_ips = ["192.168.137.227", "192.168.137.126", "192.168.137.179"]
+drone_names = {"192.168.137.227": "drone1", "192.168.137.126": "drone2", "192.168.137.179": "drone3"}
+
+cameras = [Camera(drone_names[ip], ip=ip) for ip in drone_ips]
+pioneers = [Pioneer(drone_names[ip], ip=ip) for ip in drone_ips]
 
 time.sleep(3)
- 
 
-pioneers[0].arm()
+# Поочерёдно поднимаем и опускаем каждый дрон
+for i in range(len(pioneers)):
+    pioneers[i].arm()
+
+for i in range(len(pioneers)):
+    pioneers[i].takeoff()
+    pioneers[i].go_to_local_point(0, 0, 1, 0)
+    t = time.time()
+    while not pioneers[i].point_reached():
+        if time.time() - t > 3:
+            break
+        pass
+
+
 time.sleep(5)
-pioneers[0].takeoff()
+for i in range(len(pioneers)):
+    pioneers[i].land()
+    pioneers[i].disarm()
+
 
 #
-# Поочерёдно поднимаем и опускаем каждый дрон
-# for i in range(len(pioneers)):
-#     pioneers[i].arm()
-#     time.sleep(5)
-#     pioneers[i].takeoff()
-#     time.sleep(5)
-#     pioneers[i].go_to_local_point(0.2, 0.1, 0.5, 0)
+#   for i in range(len(pioneers)):
+#     pioneers[i].go_to_local_point(0, 0, 2, 0)
 #     time.sleep(3)
-#     for j in range(3):
-#         time.sleep(1)
-#         pioneers[i].go_to_local_point(0.15 * random.random(), 0.15 * random.random(), 0.11 * random.random(), 0)
-#     pioneers[i].go_to_local_point(0, 1, 0, 0)
+#     pioneers[i].go_to_local_point(0, 0, 1, 0)
 #
 #
 # for i in range(len(pioneers)):
 #     pioneers[i].land()
 #     pioneers[i].disarm()
+
+
+# time.sleep(2)
+# for ii in range(2):
+#     for i in range(len(pioneers)):
+#         pioneer = pioneers[i]
+#         camera = cameras[i]
+#         pioneers[i].arm()
+#         time.sleep(1)
+#         pioneer.takeoff()
+#         time.sleep(2)
+#         pioneer.land()
 #
-#
-# # time.sleep(2)
-# # for ii in range(2):
-# #     for i in range(len(pioneers)):
-# #         pioneer = pioneers[i]
-# #         camera = cameras[i]
-# #         pioneers[i].arm()
-# #         time.sleep(1)
-# #         pioneer.takeoff()
-# #         time.sleep(2)
-# #         pioneer.land()
-# #
-# # for i in range(len(pioneers)):
+# for i in range(len(pioneers)):
 #     pioneers[i].disarm()
